3ba086e0b27d9c7aa68dad6f072e2a59c697e379
ardra1111 <ardra_ajay@outlook.com>
2024-06-03 11:58:46 +0300

    Performed training the model

--- src/preprocessing/preprocessors.py
+++ src/preprocessing/preprocessors.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os 
+import os
 
 from src.config import config
 
@@ -7,26 +7,22 @@ class preprocess_data:
 
     def fit(self,X,y=None):
 
-        self.num_rows =X.shape[0]
-        
-        
-        if len(X.shape) == 1:
+        self.num_rows = X.shape[0]
 
+        if len(X.shape) == 1:    
             self.num_input_features = 1
-          
         else:
             self.num_input_features = X.shape[1]
-           
-        if len(y.shape)==1:
-             self.target_feature_dim = 1
+        
+        if len(y.shape) == 1:
+            self.target_feature_dim = 1
         else:
             self.target_feature_dim = y.shape[1]
 
 
-    def transform(self, X=None,y=None):
+    def transform(self,X=None,y=None):
 
-        self.x =np.array(X).reshape(self.num_rows,self.num_input_features)
-
+        self.X = np.array(X).reshape(self.num_rows,self.num_input_features)
         self.Y = np.array(y).reshape(self.num_rows,self.target_feature_dim)
 
         return self.X, self.Y
