a708b5f4a89afe2b3e0e050716fbc4e317eb86ea
ardra1111 <ardra_ajay@outlook.com>
2024-06-03 09:48:51 +0300

    Modified train_puipeline

--- src/predict.py
+++ src/predict.py
@@ -3,4 +3,4 @@ import numpy as np
 
 from src.config import config
 import src.preprocessing.preprocessors as pp
-from src.preprocessing.data_management import load_dataset, save_model, load_model
+from src.preprocessing.data_management import load_dataset, save_model, load_model 
